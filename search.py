2e7e7e80fc32f8b72a706d281e32379fb4be72a7
2001610107 <96015934+2001610107@users.noreply.github.com>
2022-06-06 14:47:39 +0800

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 def sequential_search(lst, key):
     length = len(lst)
-    print("length:%d lst:%s" % (length, lst))
+    print(f"length:{length} lst:{lst}")
     for i in range(length):
         if lst[i] == key:
             return i
@@ -11,7 +11,7 @@ def binary_search(lst, key):
     length = len(lst)
     left = 0
     right = length - 1
-    print("length:%d lst:%s" % (length, lst))
+    print(f"length:{length} lst:{lst}")
     while left <= right:
         mid = (right + left) // 2
         if lst[mid] > key:
@@ -25,6 +25,6 @@ def binary_search(lst, key):
 
 if __name__ == '__main__':
     alst = [1, 5, 7, 8, 22, 54, 99, 123, 200, 222, 444]
-    print("The key is:%d  The location of sequential search is:%d" % (54, sequential_search(alst, 54)))
-    print("The key is:%d  The location of binary search is:%d" % (99, binary_search(alst, 99)))
+    print(f"The key is:{54}  The location of sequential search is:{sequential_search(alst, 54)}")
+    print(f"The key is:{99}  The location of binary search is:{binary_search(alst, 99)}")
 
