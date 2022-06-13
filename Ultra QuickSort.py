ee60fd887858a479d40a64d5a91029ef7ec63a40
2001610107 <96015934+2001610107@users.noreply.github.com>
2022-06-13 12:51:54 +0800

    Update Ultra QuickSort.py

--- Ultra QuickSort.py
+++ Ultra QuickSort.py
@@ -1,27 +1,34 @@
-def merge_sort(lst, left, right):
+def merge_sort(lst, tmp, left, right):
     if left >= right:
         return 0
     mid = (left + right) // 2
-    res = merge_sort(lst, left, mid) + merge_sort(lst, mid + 1, right)
-    i = left
-    j = mid + 1
-    tmp = []
+    res = merge_sort(lst, tmp, left, mid) + merge_sort(lst, tmp, mid + 1, right)
+    i, j, pos = left, mid + 1, left
     while i <= mid and j <= right:
         if lst[i] <= lst[j]:
-            tmp.append(lst[i])
+            tmp[pos] = lst[i]
             i += 1
+            res += (j - (mid + 1))
         else:
-            res += (mid - i + 1)
-            tmp.append(lst[j])
+            tmp[pos] = lst[j]
             j += 1
+        pos += 1
     while i <= mid:
-        tmp.append(lst[i])
+        tmp[pos] = lst[i]
+        res += (j - (mid + 1))
+        pos += 1
         i += 1
     while j <= right:
-        tmp.append(lst[j])
+        tmp[pos] = lst[j]
         j += 1
-    lst[left:right + 1] = tmp
+    lst[left:right + 1] = tmp[left:right + 1]
     return res
+
+
+def solution(lst):
+    n = len(lst)
+    tmp = [0] * n
+    return merge_sort(lst, tmp, 0, n - 1)
 
 
 if __name__ == '__main__':
@@ -31,6 +38,6 @@ if __name__ == '__main__':
         for i in range(n):
             m = int(input())
             lst.append(m)
-        print(merge_sort(lst, 0, n - 1))
+        print(solution(lst))
         n = int(input())
 
