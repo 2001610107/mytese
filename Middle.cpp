ba78d2e422bc57328808a0b0089ed0d45e19a23f
2001610107 <96015934+2001610107@users.noreply.github.com>
2022-06-14 22:27:35 +0800

    Update Middle.cpp

--- Middle.cpp
+++ Middle.cpp
@@ -1,3 +1,11 @@
+/*
+FJ is surveying his herd to find the most average cow. He wants to know how much milk this 'median' cow gives: half of the cows give as much or more than the median; half give as much or less.
+Given an odd number of cows N (1 <= N < 10,000) and their milk output (1..1,000,000), find the median amount of milk given such that at least half the cows give the same amount of milk or more and at least half give the same or less.
+Input
+* Line 1: A single integer N
+* Lines 2..N+1: Each line contains a single integer that is the milk output of one cow.
+*/
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
