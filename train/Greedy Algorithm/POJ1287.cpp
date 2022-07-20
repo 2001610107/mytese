4102eb763fa6459ea072d9d6a7912e3928acd8c9
2001610107 <96015934+2001610107@users.noreply.github.com>
2022-07-20 21:27:53 +0800

    POJ1287

---
+++ train/Greedy Algorithm/POJ1287.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+#define maxn 101
+#define min(a,b) a<b?a:b
+#define INF 0X3f3f3f
+int map[maxn][maxn];
+int dis[maxn];
+bool use[maxn];
+int n, m;
+void prim()
+{
+	int ans=0;
+	for (int i = 1; i <= n; i++)
+	{
+		dis[i] = map[1][i];
+		use[i] = 0;
+	}
+	use[1] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		int minx = INF;
+		int now = -1;
+		for (int j = 1; j <= n; j++)
+		{
+			if (!use[j] && dis[j] < minx)
+			{
+				now = j;
+				minx = dis[j];
+			}
+		}
+		ans += dis[now];
+		use[now] = 1;
+		for (int j = 1; j <= n; j++)
+		{
+			if (!use[j] && dis[j] >map[now][j])
+				dis[j] = map[now][j];
+		}
+	}
+	cout << ans << endl;
+}
+int main()
+{
+	int p1, p2, weight;
+	while (cin >> n && n)
+	{
+		memset(map, INF, sizeof(map));
+		cin >> m;
+		for (int i = 1; i <=m; i++)
+		{
+			cin >> p1 >> p2 >> weight;
+			map[p1][p2] = map[p2][p1] = min(map[p1][p2],weight);
+		}
+		prim();
+	}
+
+}
